c8ce4ff537a3eb26d99ae5e385271347221f6920
0xValJohn <0xvaljohn@gmail.com>
2024-04-17 17:13:06 -0400

    feat: first pass on APR oracle

--- contracts/periphery/StrategyAprOracle.sol
+++ contracts/periphery/StrategyAprOracle.sol
@@ -3,34 +3,115 @@ pragma solidity 0.8.18;
 
 import {AprOracleBase} from "@periphery/AprOracle/AprOracleBase.sol";
 
+interface IStrategy {
+    function vault() external view returns (address);
+    function staking() external view returns (address);
+    function asset() external view returns (address);
+}
+
+interface IVault {
+    function convertToShares(uint256) external view returns (uint256);
+    function expectedLiquidity() external view returns (uint256);
+    function baseInterestRate() external view returns (uint256);
+    function totalBorrowed() external view returns (uint256);
+    function quotaRevenue() external view returns (uint256);
+    function withdrawFee() external view returns (uint256);
+    function supplyRate() external view returns (uint256);
+}
+
+interface IStaking {
+    function totalSupply() external view returns (uint256);
+    function farmInfo() external view returns (uint40, uint32, uint184, uint256);
+}
+
+interface ICurvePool {
+    function price_oracle() external view returns (uint256);
+}
+
+interface IChainLink {
+    function latestRoundData()
+        external
+        view
+        returns (uint80 roundId, int256 answer, uint256 startedAt, uint256 updatedAt, uint80 answeredInRound);
+}
+
 contract StrategyAprOracle is AprOracleBase {
+
+    // Curve pool for GEAR pricing
+    address internal constant GEAR_ETH_CURVE_POOL = 0x0E9B5B092caD6F1c5E6bc7f89Ffe1abb5c95F1C2;
+
+    // Tokens
+    address internal constant WETH = 0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2;
+    address internal constant WBTC = 0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599;
+
+    // ChainLink feeds
+    address internal constant CL_WETH_USD = 0x5f4eC3Df9cbd43714FE2740f5E3616155c5b8419;
+    address internal constant CL_BTC_USD = 0xdeb288F737066589598e9214E782fa5A8eD689e8;
+
+    // Gearbox constants
+    uint256 internal constant RAY = 1e27;
+    uint16 internal constant PERCENTAGE_FACTOR = 1e4;
+    uint256 internal constant SECONDS_PER_YEAR = 31536000;
+
     constructor() AprOracleBase("Strategy Apr Oracle Example", msg.sender) {}
 
     /**
-     * @notice Will return the expected Apr of a strategy post a debt change.
-     * @dev _delta is a signed integer so that it can also represent a debt
-     * decrease.
-     *
-     * This should return the annual expected return at the current timestamp
-     * represented as 1e18.
-     *
-     *      ie. 10% == 1e17
-     *
-     * _delta will be == 0 to get the current apr.
-     *
-     * This will potentially be called during non-view functions so gas
-     * efficiency should be taken into account.
-     *
      * @param _strategy The token to get the apr for.
      * @param _delta The difference in debt.
-     * @return . The expected apr for the strategy represented as 1e18.
+     * @return The expected apr for the strategy represented as 1e18.
      */
-    function aprAfterDebtChange(
-        address _strategy,
-        int256 _delta
-    ) external view override returns (uint256) {
-        // TODO: Implement any necessary logic to return the most accurate
-        //      APR estimation for the strategy.
-        return 1e17;
+    function aprAfterDebtChange(address _strategy, int256 _delta) external view override returns (uint256) {
+        IStrategy strategy = IStrategy(_strategy);
+        IVault vault = IVault(strategy.vault());
+        IStaking staking = IStaking(strategy.staking());
+
+        // Step 1: Calculate native yield
+        uint256 assets = vault.expectedLiquidity() + uint256(_delta);
+        uint256 baseInterestRate_ = vault.baseInterestRate();
+        uint256 nativeYield = (baseInterestRate_ * vault.totalBorrowed() + vault.quotaRevenue() * RAY)
+            * (PERCENTAGE_FACTOR - vault.withdrawFee()) / PERCENTAGE_FACTOR / assets;
+
+        // Step 2: Get exchange rate for the asset
+        uint256 exchangeRate;
+        if (strategy.asset() == WETH) {
+            exchangeRate = 1e18; // 1 WETH = 1e18 (1 ETH)
+        } else if (strategy.asset() == WBTC) {
+            // Get latest BTC/USD and ETH/USD prices from ChainLink
+            (, int256 btcPrice,,,) = IChainLink(CL_BTC_USD).latestRoundData();
+            (, int256 ethPrice,,,) = IChainLink(CL_WETH_USD).latestRoundData();
+            exchangeRate = (uint256(btcPrice) * 1e18) / uint256(ethPrice); // Convert BTC price to ETH price
+        } else {
+            // Assumes other assets are stablecoins at 1:1 with ETH
+            (, int256 price,,,) = IChainLink(CL_WETH_USD).latestRoundData();
+            exchangeRate = uint256(price);
+        }
+
+        // Get GEAR price in the asset from Curve pool
+        uint256 gearInAssetPrice = ICurvePool(GEAR_ETH_CURVE_POOL).price_oracle() * exchangeRate;
+
+        // Get farm information from staking contract
+        (uint40 farmFinishTime, uint32 farmDuration, uint184 farmReward,) = staking.farmInfo();
+
+        if (farmFinishTime <= block.timestamp) {
+            // Farm is finished, no reward yield
+            return 0;
+        }
+        if (farmDuration == 0) {
+            // Farm duration is zero, no reward yield
+            return 0;
+        }
+
+        // Calculate total supply including the delta converted to shares
+        uint256 supply = staking.totalSupply() + vault.convertToShares(uint256(_delta));
+
+        // Calculate reward in asset price
+        uint256 rewardInAssetPrice = uint256(farmReward) * gearInAssetPrice / 1e18;
+
+        // Calculate reward yield
+        uint256 rewardYield =
+            (PERCENTAGE_FACTOR * rewardInAssetPrice * SECONDS_PER_YEAR) / (supply * uint256(farmDuration));
+
+        // Return total APR (native yield + reward yield)
+        return nativeYield + rewardYield;
     }
 }
