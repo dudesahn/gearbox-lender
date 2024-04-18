32f60087d5f9fe4e81a7c1e340b01d0446a92467
0xValJohn <0xvaljohn@gmail.com>
2024-04-18 10:50:58 -0400

    feat: more progress on APR oracle

--- contracts/periphery/StrategyAprOracle.sol
+++ contracts/periphery/StrategyAprOracle.sol
@@ -3,6 +3,11 @@ pragma solidity 0.8.18;
 
 import {AprOracleBase} from "@periphery/AprOracle/AprOracleBase.sol";
 
+import "forge-std/console2.sol"; // @todo: remove all logs
+
+// example of FE APY calcs: https://github.com/Gearbox-protocol/sdk/blob/next/src/gearboxRewards/apy.ts
+// https://github.com/Gearbox-protocol/defillama/blob/7127e015b2dc3f47043292e8801d01930560003c/src/yield-server/index.ts#L242
+
 interface IStrategy {
     function vault() external view returns (address);
     function staking() external view returns (address);
@@ -11,6 +16,7 @@ interface IStrategy {
 
 interface IVault {
     function convertToShares(uint256) external view returns (uint256);
+    function convertToAssets(uint256) external view returns (uint256);
     function expectedLiquidity() external view returns (uint256);
     function baseInterestRate() external view returns (uint256);
     function totalBorrowed() external view returns (uint256);
@@ -66,28 +72,35 @@ contract StrategyAprOracle is AprOracleBase {
         IStaking staking = IStaking(strategy.staking());
 
         // Step 1: Calculate native yield
-        uint256 assets = vault.expectedLiquidity() + uint256(_delta);
-        uint256 baseInterestRate_ = vault.baseInterestRate();
-        uint256 nativeYield = (baseInterestRate_ * vault.totalBorrowed() + vault.quotaRevenue() * RAY)
-            * (PERCENTAGE_FACTOR - vault.withdrawFee()) / PERCENTAGE_FACTOR / assets;
+        uint256 assets;
+        if (_delta < 0) {
+            assets = vault.expectedLiquidity() - uint256(-_delta);
+        } else {
+            assets = vault.expectedLiquidity() + uint256(_delta);
+        }
+
+        uint256 nativeYield = (vault.baseInterestRate() * vault.totalBorrowed() + vault.quotaRevenue() * RAY)
+            * (PERCENTAGE_FACTOR - vault.withdrawFee()) / PERCENTAGE_FACTOR / assets / 1e9;
 
         // Step 2: Get exchange rate for the asset
         uint256 exchangeRate;
         if (strategy.asset() == WETH) {
-            exchangeRate = 1e18; // 1 WETH = 1e18 (1 ETH)
+            exchangeRate = 1e18;
         } else if (strategy.asset() == WBTC) {
             // Get latest BTC/USD and ETH/USD prices from ChainLink
             (, int256 btcPrice,,,) = IChainLink(CL_BTC_USD).latestRoundData();
             (, int256 ethPrice,,,) = IChainLink(CL_WETH_USD).latestRoundData();
             exchangeRate = (uint256(btcPrice) * 1e18) / uint256(ethPrice); // Convert BTC price to ETH price
-        } else {
+        } else { 
             // Assumes other assets are stablecoins at 1:1 with ETH
             (, int256 price,,,) = IChainLink(CL_WETH_USD).latestRoundData();
-            exchangeRate = uint256(price);
+            exchangeRate = 1e26 / uint256(price); // ChainLink returns 8 decimals, normalize to 1e18
         }
 
+        // @todo need to consider stable with 6 decimals
+
         // Get GEAR price in the asset from Curve pool
-        uint256 gearInAssetPrice = ICurvePool(GEAR_ETH_CURVE_POOL).price_oracle() * exchangeRate;
+        uint256 gearInAssetPrice = ICurvePool(GEAR_ETH_CURVE_POOL).price_oracle() * exchangeRate / 1e18;
 
         // Get farm information from staking contract
         (uint40 farmFinishTime, uint32 farmDuration, uint184 farmReward,) = staking.farmInfo();
@@ -102,14 +115,21 @@ contract StrategyAprOracle is AprOracleBase {
         }
 
         // Calculate total supply including the delta converted to shares
-        uint256 supply = staking.totalSupply() + vault.convertToShares(uint256(_delta));
-
+        uint256 supply;
+        if (_delta < 0) {
+            supply = staking.totalSupply() - vault.convertToShares(uint256(-_delta));
+        } else {
+            supply = staking.totalSupply() + vault.convertToShares(uint256(_delta));
+        }
+        
         // Calculate reward in asset price
         uint256 rewardInAssetPrice = uint256(farmReward) * gearInAssetPrice / 1e18;
 
+        // Calculate duration ratio
+        uint256 durationRatio = (SECONDS_PER_YEAR * 1e18) / uint256(farmDuration);
+
         // Calculate reward yield
-        uint256 rewardYield =
-            (PERCENTAGE_FACTOR * rewardInAssetPrice * SECONDS_PER_YEAR) / (supply * uint256(farmDuration));
+        uint256 rewardYield = ((rewardInAssetPrice * 1e18) / vault.convertToAssets(supply)) * durationRatio / 1e18;
 
         // Return total APR (native yield + reward yield)
         return nativeYield + rewardYield;
